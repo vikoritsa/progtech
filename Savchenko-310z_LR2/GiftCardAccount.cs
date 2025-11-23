7692f834676fdb4d8d507715518bd155d4027389
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 18:00:57 +0700

    Было сделано: * Изменен конструктор для производного класса GiftCardAccount * Переропределен метод PerformMonthEndTransactions для класса GiftCardAccount

--- Savchenko-310z_LR2/GiftCardAccount.cs
+++ Savchenko-310z_LR2/GiftCardAccount.cs
@@ -2,7 +2,15 @@
 
 public class GiftCardAccount : BankAccount
 {
-    public GiftCardAccount(string name, decimal initialBalance) : base(name, initialBalance)
+    private readonly decimal _monthlyDeposit = 0m;
+
+    public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance)
+        => _monthlyDeposit = monthlyDeposit;
+    public override void PerformMonthEndTransactions()
     {
+        if (_monthlyDeposit != 0)
+        {
+            MakeDeposit(_monthlyDeposit, DateTime.Now, "Add monthly deposit");
+        }
     }
 }
