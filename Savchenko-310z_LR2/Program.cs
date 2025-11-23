72a1c702a694b67739fd40bb00c6537bbdf438e3
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 18:13:22 +0700

    Было сделано: * Добавлен код для транзакций с производными классами GiftCardAccount и InterestEarningAccount

--- Savchenko-310z_LR2/Program.cs
+++ Savchenko-310z_LR2/Program.cs
@@ -7,6 +7,22 @@ Console.WriteLine(account.Balance);
 account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
 Console.WriteLine(account.Balance);
 Console.WriteLine(account.GetAccountHistory());
+
+var giftCard = new GiftCardAccount("gift card", 100, 50);
+giftCard.MakeWithdrawal(20, DateTime.Now, "get expensive coffee");
+giftCard.MakeWithdrawal(50, DateTime.Now, "buy groceries");
+giftCard.PerformMonthEndTransactions();
+// can make additional deposits:
+giftCard.MakeDeposit(27.50m, DateTime.Now, "add some additional spending money");
+Console.WriteLine(giftCard.GetAccountHistory());
+
+var savings = new InterestEarningAccount("savings account", 10000);
+savings.MakeDeposit(750, DateTime.Now, "save some money");
+savings.MakeDeposit(1250, DateTime.Now, "Add more savings");
+savings.MakeWithdrawal(250, DateTime.Now, "Needed to pay monthly bills");
+savings.PerformMonthEndTransactions();
+Console.WriteLine(savings.GetAccountHistory());
+
 // Test for a negative balance.
 try
 {
