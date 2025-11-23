0c07b38c6258ea5309e436ec6f3dedabd967100e
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 12:31:00 +0700

    Было сделано: * Реализованы методы MakeDeposit и MakeWithdrawal * Добавлены исключения к методам MakeDeposit и MakeWithdrawal

--- Savchenko_310z_LR1/BankAccount.cs
+++ Savchenko_310z_LR1/BankAccount.cs
@@ -30,10 +30,27 @@ public class BankAccount
     private List<Transaction> _allTransactions = new List<Transaction>();
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+        }
+        var deposit = new Transaction(amount, date, note);
+        _allTransactions.Add(deposit);
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+        }
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+        var withdrawal = new Transaction(-amount, date, note);
+        _allTransactions.Add(withdrawal);
     }
+
 }
 
