f3f2e336a79b1afe95207df2f2a0ca95f843802a
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 12:15:08 +0700

    Было сделано: * Вызов конструктора в файле Program.cs

--- Savchenko_310z_LR1/Program.cs
+++ Savchenko_310z_LR1/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using Classes;
+
+var account = new BankAccount("Viktoria", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
