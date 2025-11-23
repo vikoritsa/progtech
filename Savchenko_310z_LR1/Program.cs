15da0b5852078e59e24020a0e6c58779cfd27f42
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 12:40:04 +0700

    Было сделано: * Проверка кода, вызывающего исключение при отрицательном балансе

--- Savchenko_310z_LR1/Program.cs
+++ Savchenko_310z_LR1/Program.cs
@@ -6,6 +6,16 @@ account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
 Console.WriteLine(account.Balance);
 account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
 Console.WriteLine(account.Balance);
+// Test for a negative balance.
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
 // Test that the initial balances must be positive.
 BankAccount invalidAccount;
 try
@@ -18,3 +28,4 @@ catch (ArgumentOutOfRangeException e)
     Console.WriteLine(e.ToString());
     return;
 }
+
