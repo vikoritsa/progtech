53b36d4b847fad2d6a063e470a805b9a14099a46
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 12:28:02 +0700

    Было создано: * Вычисление Balance

--- Savchenko_310z_LR1/BankAccount.cs
+++ Savchenko_310z_LR1/BankAccount.cs
@@ -5,12 +5,24 @@ public class BankAccount
     private static int s_accountNumberSeed = 1234567890;
     public string Number { get; }
     public string Owner { get; set; }
-    public decimal Balance { get; }
+    public decimal Balance
+    {
+        get
+        {
+            decimal balance = 0;
+            foreach (var item in _allTransactions)
+            {
+                balance += item.Amount;
+            }
 
-    public BankAccount(string name, decimal initialBalance)
+            return balance;
+        }
+    }
+
+
+    public BankAccount(string name)
     {
         Owner = name;
-        Balance = initialBalance;
         Number = s_accountNumberSeed.ToString();
         s_accountNumberSeed++;
 
--- Savchenko_310z_LR1/Program.cs
+++ Savchenko_310z_LR1/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Classes;
 
-var account = new BankAccount("Viktoria", 1000);
+var account = new BankAccount("Viktoria");
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
