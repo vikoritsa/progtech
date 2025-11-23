3245a2d9d027ebbe3bc9d1c736665efa975dbe2c
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 12:08:17 +0700

    Было сделано: * Создан файл BankAccount.cs * Создано пространство имен для классов * Определен класс BankAccount * Созданы 3 свойства и 2 метода класса

---
+++ Savchenko_310z_LR1/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace Classes;
+
+public class BankAccount
+{
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+    }
+}
+
