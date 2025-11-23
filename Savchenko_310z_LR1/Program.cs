72d3dc192fc1fdde4867d4527293266f73b9f88f
vikoritsa <v.savchenko95@sibstrin.ru>
2025-11-23 12:37:12 +0700

    Было сделано: * Проверка перехвата ошибок

--- Savchenko_310z_LR1/Program.cs
+++ Savchenko_310z_LR1/Program.cs
@@ -6,3 +6,15 @@ account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
 Console.WriteLine(account.Balance);
 account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
 Console.WriteLine(account.Balance);
+// Test that the initial balances must be positive.
+BankAccount invalidAccount;
+try
+{
+    invalidAccount = new BankAccount("invalid", -55);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
